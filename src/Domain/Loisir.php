d762eb090ed1bf3b8cfcca95053c0923a6a581c4
charlzyy <charles.bergheaud@lepoles.com>
2017-08-21 09:32:23 +0200

    sitecv

---
+++ src/Domain/Loisir.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MicroCMS\Domain;
+
+class Loisir
+{
+    /**
+     * Experience id.
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * Experience title.
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Experience content.
+     *
+     * @var string
+     */
+    private $content;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getContent() {
+        return $this->content;
+    }
+
+    public function setContent($content) {
+        $this->content = $content;
+        return $this;
+    }
+}
